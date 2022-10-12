4ed4351e4b23ea5de1c34f0b550daf7866ed327b
Jônatas Araújo <jonatasaraujo2257@gmail.com>
2022-10-12 15:40:47 -0300

    💩 Weird Stuff

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,25 @@
+use std::io;
+
 fn main() {
-  println!("Hello, world!");
+  println!("Choose a letter:");
+  let mut letter = String::new();
+
+  io::stdin()
+    .read_line(&mut letter)
+    .expect("Failed to read letter");
+
+  println!("You choosed ther letter: {letter}");
+
+  println!("Address of argument is {:p}", &letter);
+
+  memo(&mut letter);
+
+  println!("Value: {letter}");
+  println!("Address of argument is {:p}", &letter);
+}
+
+fn memo(s: &mut String){
+  println!("Address of argument is {:p}", s);
+  *s = String::from("oi");
+
 }
