d7e0b471619e8cd77eccb37851602bdddb547d98
Jônatas Araújo <jonatasaraujo2257@gmail.com>
2022-12-10 19:53:48 -0300

    some edits

--- src/main.rs
+++ src/main.rs
@@ -1,25 +1,7 @@
-use std::io;
+// use std::io;
+
+mod graphs;
 
 fn main() {
-  println!("Choose a letter:");
-  let mut letter = String::new();
-
-  io::stdin()
-    .read_line(&mut letter)
-    .expect("Failed to read letter");
-
-  println!("You choosed ther letter: {letter}");
-
-  println!("Address of argument is {:p}", &letter);
-
-  memo(&mut letter);
-
-  println!("Value: {letter}");
-  println!("Address of argument is {:p}", &letter);
-}
-
-fn memo(s: &mut String){
-  println!("Address of argument is {:p}", s);
-  *s = String::from("oi");
-
+  graphs::graph
 }
